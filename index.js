08c5288cb31f55a1669af20681376a3d7014df9e
Andrew Huntington <andrew.huntington@gmail.com>
2021-05-10 21:23:51 +0900

    Add chalk to project, finsh code

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 
 // Get access to the File System Module
 const fs = require("fs");
+// Chalk (NPM package) styles terminal strings
+const chalk = require("chalk");
+// path is part of the nodejs standard library
+const path = require("path");
 
 // Provides an lstat method that returns a Promise
 const { lstat } = fs.promises;
 
 // process.cwd() returns the current working dir
 // the Process Module is automatically added to the global scope of every project
-fs.readdir(process.cwd(), async (err, filenames) => {
+// process.argv is an array of arguements passed to the program from the CLI
+// User arguments start at index 2
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     // error handling code
     console.log(err);
@@ -19,7 +27,7 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 
   // Creates an array of Promises to be processed by Promise.all()
   const statPromises = filenames.map((filename) => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   // Waits for all promises to resolve, then spits out a new array w/data inside
@@ -28,6 +36,10 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   for (const stats of allStats) {
     const index = allStats.indexOf(stats);
 
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold.greenBright(filenames[index]));
+    }
   }
 });
